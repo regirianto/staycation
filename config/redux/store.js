79693148721aa211cb4752767714c4e6fd736960
unknown <regirianto22@gmail.com>
2022-03-18 14:22:38 +0700

    tes deploy in vercel

--- config/redux/store.js
+++ config/redux/store.js
@@ -1,7 +1,8 @@
 import { createStore } from "redux";
 import reducer from "./reducer";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(reducer, composeWithDevTools());
+// const store = createStore(reducer, composeWithDevTools());
+const store = createStore(reducer);
 
 export default store;
